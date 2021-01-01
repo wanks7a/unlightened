6afd51e832a5443581dc7cb6771041e74a4393c5
wanks7a <wanks7a@gmail.com>
2021-01-01 12:12:11 +0200

    added serialization for batch_norm layer

--- unlightened/include/batch_norm.h
+++ unlightened/include/batch_norm.h
@@ -5,6 +5,26 @@
 template <typename Device>
 class batch_norm : public  device_layer<batch_norm<Device>, Device, float, true>
 {
+    template <typename Serializer>
+    void serialize_weights_props(Serializer& s, const weights_properties& p) const
+    {
+        std::vector<float> data;
+        data.resize(p.size);
+        Device::copy_to_host(data.data(), p.ptr, p.size);
+        s << data;
+    }
+
+    template <typename Serializer>
+    bool deserialize_weights_props(Serializer& s, const weights_properties& p) const
+    {
+        std::vector<float> data;
+        s >> data;
+        if (data.size() != p.size)
+            return false;
+        Device::copy_to_device(p.ptr, data.data(), p.size);
+        return true;
+    }
+
 public:
 
     virtual void device_forward() = 0;
@@ -14,12 +34,16 @@ public:
     template <typename Serializer>
     void serialize_members(Serializer& s) const
     {
-
+        serialize_weights_props(s, get_weights());
+        serialize_weights_props(s, get_bias());
     }
 
     template <typename Serializer>
     bool deserialize_members(Serializer& s)
     {
-        return true;
+        init(input_shape);
+        if (!deserialize_weights_props(s, get_weights()))
+            return false;
+        return deserialize_weights_props(s, get_bias());
     }
 };
